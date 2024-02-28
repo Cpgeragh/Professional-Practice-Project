d7e1f8a569645d1a5d488c615a3e08c50f3704f4
James <g00310428@atu.ie>
2024-02-28 20:16:48 +0000

    Login function built/404 error

--- app/route.py
+++ app/route.py
@@ -1,12 +1,11 @@
 from flask import render_template, request, redirect, url_for, session
-from app.models import User, Restaurant  # Adjusted import paths
+from app.models import User  
 from app import app
-
 
 # Home page route
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 # Login route
 @app.route('/login', methods=['GET', 'POST'])
@@ -18,75 +17,12 @@ def login():
 
         # Check if the username and password are valid
         user = User.find(username)
-        if user and user['password'] == password:
+        if user and user.verify_password(password):
             session['username'] = username
             return redirect(url_for('menu'))
         else:
-            return render_template('login.html', error='Invalid username or password')
+            # If login fails, redirect to the same page (login.html)
+            return redirect(url_for('login'))
 
+    # If request method is GET or login fails, render the login page
     return render_template('login.html')
-
-# Signup route
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        # Get username and password from the form
-        username = request.form['username']
-        password = request.form['password']
-
-        # Check if the username already exists
-        if User.find(username):
-            return render_template('signup.html', error='Username already exists')
-
-        # Create a new user
-        user = User(username, password)
-        user.save()
-
-        session['username'] = username
-        return redirect(url_for('menu'))
-
-    return render_template('signup.html')
-
-# Menu route
-@app.route('/menu')
-def menu():
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    return render_template('menu.html')
-
-# Restaurants route
-# @app.route('/restaurants')
-# def restaurants():
-#     if 'username' not in session:
-#         return redirect(url_for('login'))
-    
-#     # Fetch restaurant information from MongoDB or API
-#     restaurants = Restaurant.all()
-    
-#     return render_template('restaurants.html', restaurants=restaurants)
-
-# Cinema route (similar to restaurants route)
-# @app.route('/cinema')
-# def cinema():
-#     if 'username' not in session:
-#         return redirect(url_for('login'))
-    
-#     # Fetch cinema information from MongoDB or API
-    
-#     return render_template('cinema.html')
-
-# Events route (similar to restaurants route)
-# @app.route('/events')
-# def events():
-#     if 'username' not in session:
-#         return redirect(url_for('login'))
-    
-#     # Fetch events information from MongoDB or API
-    
-#     return render_template('events.html')
-
-# Logout route
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('home'))
